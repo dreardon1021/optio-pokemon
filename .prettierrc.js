543733acffb19d3a2c5148c3f510600c9479fa63
Dan Reardon <dreardon1021@gmail.com>
2021-01-14 19:46:58 -0700

    Remove comments in prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,3 @@
-// {
-//   "printWidth": 80,
-//   "tabWidth": 2,
-//   "singleQuote": true,
-//   "trailingComma": "es5",
-//   "semi": true,
-//   "newline-before-return": true,
-//   "no-duplicate-variable": [true, "check-parameters"],
-//   "no-var-keyword": true
-// }
-
 module.exports = {
   semi: true,
   trailingComma: "all",
