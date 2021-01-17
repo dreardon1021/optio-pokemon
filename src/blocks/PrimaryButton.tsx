43e61c11f26d0cceb398f920736cc15372991bb3
Dan Reardon <dreardon1021@gmail.com>
2021-01-17 12:10:00 -0700

    Add button styling

--- src/blocks/PrimaryButton.tsx
+++ src/blocks/PrimaryButton.tsx
@@ -7,7 +7,8 @@ interface Props {
 }
 
 const PrimaryButton: FC<Props> = ({ className, children, type }) => {
-  const baseClasses = "border-2 border-black border-solid rounded-lg";
+  const baseClasses =
+    "border-2 border-black border-solid rounded-lg bg-pokemon-theme-yellow w-24 font-semibold shadow-sm cursor-pointer hover:opacity-75 focus:opacity-75";
   const classes = appendOptionalClasses(baseClasses, className);
   return (
     <button type={type} className={classes}>
