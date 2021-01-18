939817f2fcb01795edf5a28af09e1b429dc76597
Dan Reardon <dreardon1021@gmail.com>
2021-01-17 20:02:50 -0700

    Add flex to main html element for proper spacing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React, { FC } from "react";
 import { Route, Switch } from "react-router-dom";
 import GetPokeList from "./components/PokeList/GetPokeList";
 import NavBar from "./components/NavBar/NavBar";
-import PokemonDetail from "./components/PokemonDetail/PokemonDetail";
+import PokemonDetailCard from "./components/PokemonDetailCard/PokemonDetail";
 
 const App: FC = () => {
   return (
-    <main>
+    <main className="flex flex-col items-center">
       <NavBar />
       <Switch>
         <Route exact path="/" component={GetPokeList} />
-        <Route exact path="/:name" render={(props) => <PokemonDetail {...props} />} />
+        <Route exact path="/:name" render={(props) => <PokemonDetailCard {...props} />} />
       </Switch>
     </main>
   );
